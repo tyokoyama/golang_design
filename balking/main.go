06e694f91617984f110c41d1be907244ba64dd50
Takashi Yokoyama <tksyokoyama@gmail.com>
2013-02-21 15:16:06 +0900

    4章のサンプルを追加

---
+++ balking/main.go
@@ -0,0 +1,82 @@
+// Copyright 2012 T.Yokoyama All rights reserved.
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"sync"
+	"time"
+)
+
+type Data struct {
+	filename string	// ファイル名
+	content string	// データの内容
+	changed bool	// 変更の有無
+	mutex sync.Mutex
+}
+
+func (data *Data) Change(newContent string) {
+	data.mutex.Lock()
+	defer data.mutex.Unlock()
+	data.content = newContent
+	data.changed = true
+}
+
+func (data *Data) Save(name string) {
+	data.mutex.Lock()
+	defer data.mutex.Unlock()
+
+	if(!data.changed) {
+		return;
+	}
+
+	data.doSave(name)
+	data.changed = false
+}
+
+func (data Data)doSave(name string) {
+	fmt.Printf("%s Called doSave, content = %s\n", name, data.content)
+	file, err := os.OpenFile(data.filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, os.ModePerm)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer file.Close()
+
+	if _, err := fmt.Fprintf(file, "%s", data.content); err != nil {
+		panic(err.Error())
+	}
+}
+
+type SaverThread struct {
+	data *Data
+}
+
+func (thread SaverThread) Start() {
+	for {
+		thread.data.Save("SaverThread")
+		time.Sleep(1 * time.Second)
+	}
+}
+
+type ChangerThread struct {
+	data *Data
+	random *rand.Rand
+}
+
+func (thread ChangerThread) Start() {
+	for i := 0; ; i++ {
+		thread.data.Change(fmt.Sprintf("No. %d\n", i))
+		time.Sleep(time.Duration(thread.random.Int31()))
+		thread.data.Save("ChangerThread")
+	}
+}
+
+func main() {
+	data := &Data {filename: "data.txt", content: "(empty)", changed: true}
+	go ChangerThread {data: data, random: rand.New(rand.NewSource(time.Now().Unix()))}.Start()
+	go SaverThread {data: data}.Start()
+
+	select {}
+}
